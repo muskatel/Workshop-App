fc802ec9ada789df676d76ad91b3c3b37263244e
Craig Marais <craig.marais.85@gmail.com>
2026-01-13 10:52:59 +0100

    Switched to using a function

--- BasicApp/Program.cs
+++ BasicApp/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        PrintHello();
+    }
+
+    static void PrintHello()
+    {
         Console.WriteLine("Hello, World!");
     }
 }
