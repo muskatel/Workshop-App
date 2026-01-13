bea27ff0d1b2bea7ee3f036d87c6082e048f067d
Craig Marais <craig.marais.85@gmail.com>
2026-01-13 11:07:23 +0100

    reworked message

--- BasicApp/Program.cs
+++ BasicApp/Program.cs
@@ -4,10 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        PrintHello();
         PrintHello("Craig");
     }
 
-    static void PrintHello(string name)
+    static void PrintHello(string name = "World!")
     {
         Console.WriteLine("Hello, {name}!");
     }
